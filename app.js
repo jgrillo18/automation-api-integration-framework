e1a6e3bfde4f2687fc832eb5d7e5b1e751838db7
jgrillo18 <jgrillo18@users.noreply.github.com>
2026-03-17 03:22:00 +0000

    deploy: 815bc9602c83d0cb894a108e57e1181cf5dd8217

--- app.js
+++
@@ -1,7 +0,0 @@
-function login(){
-    if(document.getElementById('user').value==='admin' && document.getElementById('pass').value==='admin'){
-        document.getElementById('loginScreen').style.display='none';
-        document.getElementById('dashboardScreen').style.display='block';
-    } else alert('credenciales inválidas');
-}
-function logout(){document.getElementById('dashboardScreen').style.display='none';document.getElementById('loginScreen').style.display='block';}
